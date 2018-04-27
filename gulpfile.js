607c8fe5a00c7958cc6f6621396e65f2f6d8e687
Aj <alfred@Alfreds-iMac.local>
2018-04-27 11:08:41 -0400

    move all JS files

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ var sass = require('gulp-sass');
 var browserSyn = require('browser-sync');
 var reload = browserSyn.reload;
 var prefix = require('gulp-autoprefixer');
+var clean = require('gulp-clean');
 
 var SOURCEPATHS = {
    sassSource : 'src/scss/*.scss',
-   htmlSource : 'src/*.html'
+   htmlSource : 'src/*.html',
+   jsSource : 'src/js/**'
 }
 
 
@@ -17,6 +19,19 @@ var APPATH = {
 }
 
 
+//Clean all Html codes
+gulp.task('clean-html', function(){
+  return gulp.src(APPATH.root + '/*.html', {read: false, force: true})
+  .pipe(clean());
+})
+
+
+//clean All Javascript scripts
+gulp.task('clean-scripts', function(){
+  return gulp.src(APPATH.js + '/*.js', {read: false, force: true})
+  .pipe(clean());
+})
+
 gulp.task('sass', function(){
   return gulp.src(SOURCEPATHS.sassSource)
     .pipe(prefix('last 2 versions'))
@@ -24,7 +39,14 @@ gulp.task('sass', function(){
     .pipe(gulp.dest(APPATH.css));
 });
 
-gulp.task('copy', function(){
+gulp.task('scripts', ['clean-scripts'], function(){
+  gulp.src(SOURCEPATHS.jsSource)
+    .pipe(gulp.dest(APPATH.js))
+});
+
+
+
+gulp.task('copy', ['clean-html'], function(){
   gulp.src(SOURCEPATHS.htmlSource)
   .pipe(gulp.dest(APPATH.root))
 })
@@ -37,9 +59,10 @@ gulp.task('serve', ['sass'], function(){
     })
 });
 
-gulp.task('watch', ['serve', 'sass','copy'], function(){
+gulp.task('watch', ['serve', 'sass','copy', 'clean-html','clean-scripts', 'scripts'], function(){
   gulp.watch([SOURCEPATHS.sassSource], ['sass']);
   gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
+  gulp.watch([SOURCEPATHS.jsSource], ['scripts']);
 });
 
 gulp.task('default', ['watch']);
