e31f903d31479e806e383e2eb03fc8869fb6854a
Aj <alfred@Alfreds-iMac.local>
2018-04-27 10:29:27 -0400

    added copy task in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSyn = require('browser-sync');
 var reload = browserSyn.reload;
+var prefix = require('gulp-autoprefixer');
 
 var SOURCEPATHS = {
-
-   sassSource : 'src/scss/*.scss'
+   sassSource : 'src/scss/*.scss',
+   htmlSource : 'src/*.html'
 }
 
 
@@ -18,9 +19,15 @@ var APPATH = {
 
 gulp.task('sass', function(){
   return gulp.src(SOURCEPATHS.sassSource)
+    .pipe(prefix('last 2 versions'))
     .pipe(sass({outputStyle: 'expanded'}).on('error',sass.logError))
     .pipe(gulp.dest(APPATH.css));
 });
+
+gulp.task('copy', function(){
+  gulp.src(SOURCEPATHS.htmlSource)
+  .pipe(gulp.dest(APPATH.root))
+})
 
 gulp.task('serve', ['sass'], function(){
     browserSyn.init([APPATH.css + '/*.css', APPATH.root + '/*.html', APPATH.js + '/*.js'], {
@@ -30,8 +37,9 @@ gulp.task('serve', ['sass'], function(){
     })
 });
 
-gulp.task('watch', ['serve', 'sass'], function(){
+gulp.task('watch', ['serve', 'sass','copy'], function(){
   gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+  gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
 });
 
 gulp.task('default', ['watch']);
