10cb40d336ce813e00076c866ad5f34d001a71eb
Aj <alfred@Alfreds-iMac.local>
2018-04-27 09:55:04 -0400

    BrowserSync added

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,37 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSyn = require('browser-sync');
+var reload = browserSyn.reload;
+
+var SOURCEPATHS = {
+
+   sassSource : 'src/scss/*.scss'
+}
+
+
+var APPATH = {
+  root: 'app/',
+  css : 'app/css',
+  js : 'app/js'
+}
 
 
 gulp.task('sass', function(){
-  return gulp.src('src/scss/app.scss')
+  return gulp.src(SOURCEPATHS.sassSource)
     .pipe(sass({outputStyle: 'expanded'}).on('error',sass.logError))
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest(APPATH.css));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('serve', ['sass'], function(){
+    browserSyn.init([APPATH.css + '/*.css', APPATH.root + '/*.html', APPATH.js + '/*.js'], {
+      server: {
+        baseDir : APPATH.root
+      }
+    })
+});
+
+gulp.task('watch', ['serve', 'sass'], function(){
+  gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+});
+
+gulp.task('default', ['watch']);
